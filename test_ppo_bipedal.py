5b2040c938ed4509e7caabb7c6aaba61495fc60d
imoneoi <imone@tuta.io>
2020-05-20 14:17:08 +0800

    updated bipedal

--- test_ppo_bipedal.py
+++ test_ppo_bipedal.py
@@ -15,16 +15,18 @@ from tqdm import tqdm
 #C_avg_runs = 10
 C_avg_runs = 1
 
-H_num_epochs = 2500
+H_num_epochs = 50000
 H_steps_per_iteration = 2048
 H_test_episodes = 10
 H_repeat = 10
 H_lr = 2.5e-4
 
-def train_policy(seed):
+def train_policy(seed, device):
+    tqdm.write("Train with device " + device + " seed " + str(seed))
+
     #construct envs
     def MakeEnv():
-        return gym.make("BipedalWalker-v3")
+        return gym.make("BipedalWalkerHardcore-v3")
 
     def IsStop(reward):
         return reward >= 300
@@ -39,8 +41,7 @@ def train_policy(seed):
     test_env.seed(seed)
 
     #construct policy
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
+    device = torch.device(device)
     net = Network((24), (4), 1.0, device).to(device)
     optimizer = torch.optim.Adam(net.parameters(), lr=H_lr)
     policy = PPO(net, optimizer)
@@ -60,10 +61,17 @@ def train_policy(seed):
         result = train_collector.collect(n_step=H_steps_per_iteration)
         batch = train_collector.get_experience()
 
+        collect_time = time.time() - start_time
+
+        start_time = time.time()
+
         #train model
         train_metric = policy.train(batch, H_repeat)
 
-        total_duration += time.time() - start_time
+        train_time = time.time() - start_time
+
+        total_duration += collect_time + train_time
+        tqdm.write("Collect Time: {:.2f}s / Train time: {:.2f}s".format(collect_time, train_time))
 
         avg_metric = {}
         for k, v in train_metric.items():
@@ -71,6 +79,9 @@ def train_policy(seed):
 
         tqdm.write(str(result))
         tqdm.write(str(avg_metric))
+
+        #save model
+        torch.save(net.state_dict(), "checkpoints/ppo_bipedal_hardcore_seed_" + str(seed))
 
         #need to stop?
         if IsStop(result["rew"]):
@@ -83,9 +94,6 @@ def train_policy(seed):
     train_env.close()
     test_env.close()
 
-    #save model
-    torch.save(net.state_dict(), "checkpoints/bipedal")
-
     #visualize result
     render_env = VectorEnv([MakeEnv for _ in range(1)])
     render_collector  = Collector(render_env, policy)
@@ -93,11 +101,14 @@ def train_policy(seed):
 
     return total_duration
 
+seed = 1
+device = "cuda:0"
+
 #Average all runs
 run_time = []
 for run_id in tqdm(range(C_avg_runs)):
     torch.cuda.empty_cache()
 
-    run_time.append(train_policy(run_id))
+    run_time.append(train_policy(int(str(seed) + str(run_id)), device))
 
 tqdm.write("{}s +/- {}s".format(np.mean(run_time), np.std(run_time)))
